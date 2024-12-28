9f100ed2685defc48fcdd57963a0e8bbe58d7124
Daniel Onyeakazi <126322853+zendayasbrother@users.noreply.github.com>
2024-12-28 16:54:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import turtle  
-
-# Score
-
+import turtle
 
 #Setup
 wn = turtle.Screen()
 wn.title("PONG by Daniel (@zendayasbrother)")
 wn.bgcolor("black")
-wn.setup(width=800, height=600)
+wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
-# Ball Speed
-BALL_SPEED = 1.5
+# Score
+score_a = 0
+sore_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
@@ -27,7 +25,7 @@ paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
@@ -38,8 +36,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = BALL_SPEED
-ball.dy = BALL_SPEED
+ball.dx = 1
+ball.dy = 1
 
 # Pen for scoring
 pen = turtle.Turtle()
@@ -48,7 +46,7 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+pen.write("Player A: 0  Player B: 0", align = "center", font = ("Courier", 24, "normal"))
 
 # Functions
 def paddle_a_up():
@@ -98,10 +96,15 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align = "center", font = ("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align = "center", font = ("Courier", 24, "normal"))
+
 
     # Paddle and ball collision
     if (340 < ball.xcor() < 350) and (paddle_b.ycor() - 40 < ball.ycor() < paddle_b.ycor() + 40):
